963904d671830847f0861a577c252b8e9f6db81a
TheDuke03 <94278669+TheDuke03@users.noreply.github.com>
2025-07-26 00:54:10 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,5 +1,6 @@
 import mlflow
 import mlflow.sklearn
+import mlflow.models
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -9,8 +10,6 @@ import os
 df = pd.read_csv("MLProject/namadataset_preprocessing/winequality_preprocessed.csv")
 X = df.drop("quality_label", axis=1)
 y = df["quality_label"]
-
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 # Set experiment
@@ -18,18 +17,10 @@ mlflow.set_experiment("Wine_Autolog")
 
 with mlflow.start_run():
     mlflow.sklearn.autolog()
-
-    # Train model
     model = RandomForestClassifier()
     model.fit(X_train, y_train)
 
-    # Buat folder model output secara eksplisit
-    output_path = "MLProject/model"
-    os.makedirs(output_path, exist_ok=True)
-
-    # Simpan model ke MLProject/model agar bisa dibuild Docker
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path=output_path,
-        registered_model_name="wine_model"
-    )
+    # Save model directly to MLProject/model (NOT log_model)
+    output_dir = "MLProject/model"
+    os.makedirs(output_dir, exist_ok=True)
+    mlflow.sklearn.save_model(sk_model=model, path=output_dir)
